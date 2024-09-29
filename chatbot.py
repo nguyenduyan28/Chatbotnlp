01b19b13d6452c0fe0c95a5945fbd6fefea38853
nguyenduyan28 <nguyenduyan28@gmail.com>
2024-09-29 20:45:15 +0700

    clean remove punctuation

--- chatbot.py
+++ chatbot.py
@@ -2,20 +2,23 @@ import string
 
 
 
-
+def checkNone(list):
+	for char in list:
+		if (char == ''): return False
+		else: return True
 def preprocessing(user_input):
 	user_input = user_input.lower()
 	# remove punctuation	
-	non_punct = ''.join(list(map(lambda s: ' ' if s in string.punctuation else s , user_input)))
-	print(f"non_punct : {non_punct}")
-
+	non_punct = ''.join(list(map(lambda s: ' ' if s in string.punctuation else s , user_input))).split(' ')
+	print(non_punct)
+	non_punct = ' '.join(list(filter(checkNone, non_punct)))
 	return non_punct
 
 
 def main():
 	message = str(input("Take input something to say with chatbot: "))
 	message = preprocessing(message)
-	print(f"String with out punctuation {message}")
+	print(f"String with out punctuation:  {message}")
 
 if __name__ == '__main__':
 	main()
