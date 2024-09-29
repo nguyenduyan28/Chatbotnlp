3d5292c1c3791ac4a1e78ae2afb1dec2234b4a04
nguyenduyan28 <nguyenduyan28@gmail.com>
2024-09-29 17:52:24 +0700

    First commit

--- chatbot.py
+++ chatbot.py
@@ -6,16 +6,10 @@ import string
 def preprocessing(user_input):
 	user_input = user_input.lower()
 	# remove punctuation	
-	punct_set = set()
-	for char in user_input:
-		if (char in string.punctuation):
-			punct_set.add(char)
-		print(char for char in user_input if char in string.punctuation)		
-	# punct_set.add(char for char in user_input if char in string.punctuation)
-		# print(punct_set)
-		user_input = user_input.replace(str(punct for punct in punct_set), '')
-		
-	return user_input
+	non_punct = ''.join(list(map(lambda s: ' ' if s in string.punctuation else s , user_input)))
+	print(f"non_punct : {non_punct}")
+
+	return non_punct
 
 
 def main():
